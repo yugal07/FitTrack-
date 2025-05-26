dcbb290709b0aa1b7646d55a423db33ddd2d776f
yugal07 <yashsadhwani544@gmail.com>
2025-05-26 22:04:08 +0530

    Cors error

--- server/index.js
+++ server/index.js
@@ -21,8 +21,8 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
+    origin: '*',
+    credentials: false,
   })
 );
 app.use(express.json());
